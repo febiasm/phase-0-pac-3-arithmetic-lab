bcf74795953062cfc1a64b0fda7145b46d3ae572
febiasm <trevorfebias@gmail.com>
2022-11-19 22:36:01 +0300

    done

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a,b){
+    return a + b;
+}
+
+console .log(add(15,5));
+
+function subtract(a,b){
+    return a - b;
+}
+console.log(subtract(15,5));
+
+function subtract(b,a){
+    return b - a;
+}
+console.log(subtract(10,5))
+
+function multiply(a,b){
+    return a * b;
+}
+console.log(multiply(5,5));
+
+function divide(a,b){
+    return a / b;
+}
+console.log(divide(20,5));
+
+function increment(n){
+    return ++n;
+}
+console .log(increment(30));
+
+function decrement(n){
+    return --n;
+}
+console.log(decrement(15));
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
