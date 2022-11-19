504f45d4b83b5fe618e12d54c77ba02bb06e8f0f
febiasm <trevorfebias@gmail.com>
2022-11-19 22:41:30 +0300

    correct

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ function add(a,b){
     return a + b;
 }
 
-console .log(add(15,5));
+console .log(add(52,52));
 
 function subtract(a,b){
     return a - b;
 }
-console.log(subtract(15,5));
+console.log(subtract(122,53));
 
 function subtract(b,a){
     return b - a;
 }
-console.log(subtract(10,5))
+console.log(subtract(22,35))
 
 function multiply(a,b){
     return a * b;
 }
-console.log(multiply(5,5));
+console.log(multiply(4,44));
 
 function divide(a,b){
     return a / b;
 }
-console.log(divide(20,5));
+console.log(divide(2,44));
 
 function increment(n){
     return ++n;
 }
-console .log(increment(30));
+console .log(increment(50));
 
 function decrement(n){
     return --n;
 }
-console.log(decrement(15));
+console.log(decrement(5));
 
 function makeInt(n){
     return parseInt(n,10);
